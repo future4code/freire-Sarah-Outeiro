30e3d23350d9702904b6ea98c0db1da0c3227a18
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-05-09 17:34:40 -0300

    Adicionado Exercício de JavaScript

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,65 @@
+//Exercicios de interpretacao de codigo
+/* 1. 
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+resposta 1. (10) e (10, 5) */
+
+/* 2.
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+resposta 2. (10, 10, 10) */
+
+/* 3.
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+resposta 3. 
+let horasDiarias = prompt("Quantas horas você trabalha por dia?")
+let salarioDiario = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horasDiarias/salarioDiario} por hora`)*/
+
+//Exercicios de Escrita de código
+/* 1. 
+let nome
+let idade
+console.log(typeof nome, typeof idade)
+Resposta d). Foi impresso o tipo "Undefined" porque não dei nenhum valor para as variaveis. 
+let nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual é a sua idade?")
+console.log(typeof nome, typeof idade)
+Resposta f). O tipo das duas variáveis foram string independentemente se escrevi números ou palavras por causa do comando prompt.
+let nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual é a sua idade?")
+console.log("Olá!", nome, "você tem", idade, "anos")*/
+
+/* 2.
+let perguntaUm = "Você come carne? - SIM"
+let perguntaDois = "Você gosta de sertanejo? - Não"
+let perguntaTres = "Você assiste BBB? - Não"
+console.log (perguntaUm, perguntaDois, perguntaTres)*/
+
+/* 3.
+let a = 10
+let b = 25
+let c 
+c = a 
+a = b 
+b = c 
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+*/
+
+
+
+
