03696af4ad10a33ba4e77a4f426bde2c21d85763
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-05-18 17:29:09 -0300

    lacos adicionado

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,25 @@
+// EXERCÍCIOS DE INTERPRETACAO DE CODIGO
+/* EXERCICIO 1 
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
+
+Resposta 1: O código está primeiro contando os numeros até 5 e depois somando com o 5 o que resulta no número 10. */
+
+/* EXERCÍCIO 2 
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+} */
+// Resposta a) Ele vai imprimir no console os números maiores que 18.
+// Resposta b) 
+/* const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+} */
