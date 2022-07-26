d250930d1933c1466741a28753284a5c59ccc111
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-07-26 12:23:30 -0300

    favicon e titulo adicionados

--- Modulo 3/labeddit/labeddit/src/components/PostList/PostList.js
+++ Modulo 3/labeddit/labeddit/src/components/PostList/PostList.js
@@ -10,16 +10,16 @@ const PostList = () => {
 
     return(
         <div>
-            <Posts onClick={() => goToPostDetailPage(navigate)}>
+            <Posts>
                 <EnviadoPor>Enviado por: labeluno38</EnviadoPor>
-                <Texto>Por que a maioria dos desenvolvedores usam linux? ou as empresas de tecnologia usam linux?</Texto>
+                <Texto onClick={() => goToPostDetailPage(navigate)}>Por que a maioria dos desenvolvedores usam linux? ou as empresas de tecnologia usam linux?</Texto>
                 <DivBottom>
                     <DivEngajamento>
                     <ArrowUpwardIcon color={'fifth'} fontSize={'small'}/>
                     <p>1.2k</p> 
                     <ArrowDownwardIcon color={'fifth'} fontSize={'small'}/>
                     </DivEngajamento>
-                    <DivComentarios>
+                    <DivComentarios onClick={() => goToPostDetailPage(navigate)}>
                         <ChatBubbleOutlineIcon color={'fifth'} fontSize={'small'} />
                         <p>54</p>
                     </DivComentarios>
