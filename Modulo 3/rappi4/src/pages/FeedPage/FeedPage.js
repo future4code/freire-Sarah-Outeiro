987ca74815e862f595f98483c86baaadb0c0e016
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-08-05 16:19:48 -0300

    Layout e Componentes do Feed Prontos

--- Modulo 3/rappi4/src/pages/FeedPage/FeedPage.js
+++ Modulo 3/rappi4/src/pages/FeedPage/FeedPage.js
@@ -1,12 +1,20 @@
 import { Container, FeedContainer } from './styled'
+import SearchBar from '../../components/SearchBar/SearchBar'
+import FoodTypeList from '../../components/FoodTypeList/FoodTypeList'
+import RestaurantCard from '../../components/RestaurantCard/RestaurantCard'
 import NavBar from '../../components/NavBar/NavBar'
+import Header from '../../components/Header/Header'
 
 const FeedPage = () => {
     return (
       <Container>
-        <p>Rappi4</p>
+        <Header/>
         <FeedContainer>
-          
+          <SearchBar/>
+          <FoodTypeList/>
+          <RestaurantCard/>
+          <RestaurantCard/>
+          <RestaurantCard/>
         </FeedContainer>
         <NavBar/>
       </Container>
--- Modulo 3/rappi4/src/pages/FeedPage/styled.js
+++ Modulo 3/rappi4/src/pages/FeedPage/styled.js
@@ -2,16 +2,14 @@ import styled from "styled-components"
 
 export const Container = styled.div`
     height: auto;
-    p {
-        font-size: 16px;
-        text-align: center;
-        margin: 0;
-        padding-top: 15px;
-        padding-bottom: 15px;
-    }
 `
 export const FeedContainer = styled.div`
     border-top: 1px solid #d0d0d0;
     margin-bottom: 0;
-    height: auto;
+    height: 600px;
+    padding-top: 50px;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+    display: none;
+    }
 `
