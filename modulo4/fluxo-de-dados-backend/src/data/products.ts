0f33d03fae2df1aacd5b34352a990c75ec6a8c0d
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-08-17 20:41:52 -0300

    fluxo-de-dados-backend

---
+++ modulo4/fluxo-de-dados-backend/src/data/products.ts
@@ -0,0 +1,23 @@
+export type Product = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const products: Array<Product> = [
+    {
+        id: '1',
+        name: 'Iphone 4',
+        price: 500
+    },
+    {
+        id: '2',
+        name: 'Liquidificador',
+        price: 300
+    },    
+    {
+        id: '3',
+        name: 'Calça de Adolescente',
+        price: 301
+    },
+]
