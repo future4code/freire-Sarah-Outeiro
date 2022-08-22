0db1cd7e58f5f6f25d3ac526edf2e5b8a963a9f3
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-08-22 17:51:14 -0300

    apis-rest iniciado

--- modulo4/apis-rest/src/index.ts
+++ modulo4/apis-rest/src/index.ts
@@ -1,5 +1,6 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import cors from "cors"
+import { User, users } from "./data/users"
 
 const app = express()
 
@@ -8,4 +9,23 @@ app.use(cors())
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003")
+})
+
+app.get('/users', (req: Request, res: Response) => {
+    let codeError: number = 400
+    try {
+        const name: string = req.query.name as string
+        const user: User | undefined = users.find((user) => user.name === name)
+        if (!user) {
+            codeError = 404
+            throw new Error('User not found')
+        }
+        res.status(200).send(user)
+    } catch(error: any) {
+        res.status(codeError).send({message: error.message})
+    }
+})
+
+app.get('/', (req, res) => {
+    res.send('oi')
 })
