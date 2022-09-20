9b6d3cf963c898b5331b0bd90d299bc386289e3e
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-09-20 18:49:54 -0300

    Base feita

--- modulo4/iwfs-cookenu/src/index.ts
+++ modulo4/iwfs-cookenu/src/index.ts
@@ -1,7 +1,1 @@
 import { app } from './app'
-import Authenticator from './services/Authenticator'
-
-app.get('/teste', async (req, res) => {
-    const token = new Authenticator().generateToken('123')
-    console.log(token)
-})
