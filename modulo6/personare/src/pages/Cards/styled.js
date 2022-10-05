60742c49645b62f956335951cf7db4292dded2ee
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-10-05 18:41:44 -0300

    iniciando responsividade

--- modulo6/personare/src/pages/Cards/styled.js
+++ modulo6/personare/src/pages/Cards/styled.js
@@ -12,10 +12,20 @@ export const Titulo = styled.div`
     width: 1000px;
     text-align: center;
     margin: 0 auto;
+    @media (max-width: 500px) {
+        width: 90%;
+        height: auto;
+        flex-direction: column;
+    }
 `
 
 export const Title = styled.img`
     width: 600px;
+    @media (max-width: 500px) {
+        width: 100%;
+        height: auto;
+        margin: 20px 0 20px 0;
+    }
 `
 
 export const Mesa = styled.div`
@@ -35,4 +45,11 @@ export const Cartas = styled.div`
     grid-template-columns: repeat(40, 30px);
     grid-template-rows: 1fr 1fr;
     grid-row-gap: 20px;
+    @media (max-width: 500px) {
+        overflow-x: scroll;
+        margin: 0 auto;
+        display: flex;
+        column-gap: 10px;
+        padding: 0 15px 0 15px;
+    }
 `
--- modulo6/personare/src/pages/HomePage/styled.js
+++ modulo6/personare/src/pages/HomePage/styled.js
@@ -5,11 +5,19 @@ export const HomePageDiv = styled.div`
     text-align: center; 
     img {
         height: 500px;
+        @media (max-width: 500px) {
+            width: 100%;
+            height: auto;
+        }
     }
     p {
         color: #231F20;
         margin-top: -100px;
         margin-bottom: 100px;
         font-size: 20px;
+        @media (max-width: 500px) {
+            margin-top: -50px;
+            padding: 0 10px 0 10px;
+        }
     }
 `
